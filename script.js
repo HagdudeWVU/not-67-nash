4f132b38fee96c156b32d734bb468195a98b2018
Jude <haggerty-ju@learningacademy.education>
2026-05-05 08:32:18 -0400

    Refactor script.js to remove showTime and add pickWord
    Removed showTime function and its interval. Added pickWord function to display a random word.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+const words = ["yes", "no"];
+function pickWord() {
+  const index = Math.floor(Math.random() * words.length);
+  document.getElementById('word').innerText = fruits[index];
 }
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
